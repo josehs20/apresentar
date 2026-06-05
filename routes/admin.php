89277b3a0e92ddb3ba8b0dbbe513ac22fab7f8f7
josehs20 <josehenriquesimiquelsouza@gmail.com>
2026-06-05 00:12:54 -0300

    build

--- routes/admin.php
+++ routes/admin.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/admin', function () {
     return redirect()->route('admin.dashboard');
-})->name('login');
+})->name('admin.login');
 Route::prefix('admin')->middleware(['auth'])->name('admin.')->group(function () {
 
     // Dashboard
